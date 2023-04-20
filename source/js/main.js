6bde04305884f844a082ca50d1dd4fd551f7d7ce
Philipp Mendogralo <abkxj59@gmail.com>
2023-04-20 20:25:20 +0500

    init

---
+++ source/js/main.js
@@ -0,0 +1,9 @@
+import {initButtons} from './modules/buttons.js';
+
+// // ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('load', () => {
+    initButtons();
+  });
+});
---
+++ source/js/modules/buttons.js
@@ -0,0 +1,3 @@
+const initButtons = () => {};
+
+export {initButtons};
